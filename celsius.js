d689d5ce8f69490778644fb4afe1521764564383
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-06-28 20:42:31 +0530

    Add files via upload

---
+++ celsius.js
@@ -0,0 +1,19 @@
+var score=100
+score=130
+
+const bonus=50
+
+var finalscore=(score+bonus)* 0.8
+
+var complex= 4+4 * 5/2
+/*don't do like this,always do this
+ kind of calculations in parenthesis*/
+
+console.log(finalscore)
+console.log(complex)
+
+
+let tempinF = 93
+let tempinC= ((tempinF-32) *(5/9))
+
+console.log('temperature in celcius is '+ tempinC)
