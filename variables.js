f47752ce4016916a6ca8dabddc7a0a469a8deffe
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-06-29 09:45:46 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,12 @@
+let name= 'Rudra';
+let score=100;
+let bonus=20;
+
+let totalscore= score + bonus;
+
+console.log(totalscore );
+
+let firstname='Rudranil'
+let lastname='Maity'
+
+console.log(firstname+' '+lastname)
