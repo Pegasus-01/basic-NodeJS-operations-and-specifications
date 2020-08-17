bab50bb4802de1f494837a19fb61799931e1810d
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-08-17 09:34:16 +0530

    Add files via upload

---
+++ 02-Objects/01-myObject.js
@@ -0,0 +1,10 @@
+let myArray=['name',33,true]
+let myYTvideo={
+    title:'loops in javascript',
+    videoLength: '15',
+    videoDescription: 'this is a video description',
+    videoCreator:'Rudra'
+}
+console.log(myYTvideo)
+//if we want a specific value
+console.log(`Hi new video on ${myYTvideo.title} is created by ${myYTvideo.videoCreator}`)
