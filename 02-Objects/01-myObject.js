f74bee928b6254dc4f27580b9d40a686070421e6
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-08-17 10:49:43 +0530

    Update 01-myObject.js

--- 02-Objects/01-myObject.js
+++ 02-Objects/01-myObject.js
@@ -1,4 +1,4 @@
-let myArray=['name',33,true]
+let myArray=['name',33,true]     //just an example
 let myYTvideo={
     title:'loops in javascript',
     videoLength: '15',
@@ -8,3 +8,13 @@ let myYTvideo={
 console.log(myYTvideo)
 //if we want a specific value
 console.log(`Hi new video on ${myYTvideo.title} is created by ${myYTvideo.videoCreator}`)
+
+let changeVideoLength= function(myObject){
+    return {
+        format1:'`time of the videolength is ${myObject.videoLength + 1}`',
+        format2:'`time of the videolength is ${myObject.videoLength +2}`'
+    }
+}
+let adOne = changeVideoLength(myYTvideo)
+console.log(adOne.format1)
+//we can return a whole object by using this method
